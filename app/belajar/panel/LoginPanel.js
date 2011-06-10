1e3d7ae5649671ccba145e9a64f0e30b5149ddae
Endy Muhardin <endy.muhardin@gmail.com>
2011-06-10 09:54:25 +0700

    create login box

---
+++ app/belajar/panel/LoginPanel.js
@@ -0,0 +1,48 @@
+Ext.define('belajar.panel.LoginPanel', {
+    extend: 'Ext.form.Panel',
+    
+    id: 'loginForm',
+    
+    frame: true, 
+    title: 'Login',
+    height: 200,
+    width: 400,
+    
+    fieldDefaults: {
+        labelAlign: 'left',
+        labelWidth: 90,
+        anchor: '100%'
+    },
+    
+    items: [{
+        xtype:'box'
+        ,anchor:''
+        ,fieldLabel:'Image'
+        ,autoEl:{
+            tag:'div', 
+            style: 'text-align:center',
+            children:[{
+                tag:'img'
+                ,qtip:'ArtiVisi Intermedia'
+                ,src:'./resources/img/artivisi.png'
+            }]
+        }
+    },{
+        xtype: 'textfield',
+        name: 'j_username',
+        fieldLabel: 'Username',
+        value: ''
+    }, {
+        xtype: 'textfield',
+        name: 'j_password',
+        inputType: 'password',
+        fieldLabel: 'Password'
+    }],
+    
+    buttons: [{
+        text: 'Login', 
+        handler: function(){
+            Ext.getCmp('loginScreen').close();
+        }
+    }]
+});
---
+++ login.js
@@ -0,0 +1,25 @@
+// Register namespaces and their corresponding paths to Ext.Loader
+Ext.Loader.setPath({
+    'belajar': 'app/belajar'
+});
+
+// Specify a list of classes your application your application needs
+Ext.require([
+    'Ext.container.Viewport',
+    'belajar.panel.LoginPanel'
+]);
+
+// Application's initialization
+Ext.onReady(function() {
+    var win = Ext.create('Ext.Window', {
+        id: 'loginScreen',
+        layout: 'fit',
+        plain: true,
+        closable: false,
+        items: [
+            new belajar.panel.LoginPanel()
+        ]
+    });
+    win.show();
+});
+
