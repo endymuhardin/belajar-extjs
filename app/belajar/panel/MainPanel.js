a76104963222460eccd3ae50fe852eb6b5555ee5
Endy Muhardin <endy.muhardin@gmail.com>
2011-06-10 11:33:39 +0700

    implement tab panel in the center

--- app/belajar/panel/MainPanel.js
+++ app/belajar/panel/MainPanel.js
@@ -1,77 +1,65 @@
 Ext.define('belajar.panel.MainPanel', {
-    extend: 'Ext.panel.Panel',
-    frame: true, 
-    layout: 'border', 
-    
-    items: [
-        {
-            xtype: 'toolbar', 
-            region: 'north', 
-            height: 30,
-            items: [
-                {
-                    text: 'System', 
-                    menu: {
-                        xtype: 'menu',
-                        plain: true, 
-                        items: [
-                            {
-                                text: 'Ganti Password'
-                            }, 
-                            {
-                                text: 'Logout'
-                            }
-                        ]
-                    }
-                }, 
-                {
-                    text: 'Master Data', 
-                    menu: {
-                        xtype: 'menu',
-                        plain: true, 
-                        items: [
-                            {
-                                text: 'User'
-                            }, 
-                            {
-                                text: 'Role'
-                            },
-                            {
-                                text: 'Permission'
-                            }
-                        ]
-                    }
-                }, 
-                {
-                    text: 'Transaksi'
-                }, 
-                {
-                    text: 'Report'
-                }
-            ]
-        }, 
-        {
-            xtype: 'panel',
-            title: 'Information',
-            collapsible: true, 
-            region: 'east', 
-            width: 300
-        }, 
-        {
-            xtype: 'toolbar',
-            region: 'south', 
-            height: 20, 
-            items: [
-                {
-                    text: 'current user : endy', 
-                    xtype: 'label'
-                }
-            ]
-        }, 
-        {
-            xtype: 'panel',
-            html   : 'Screen', 
-            region: 'center'
-        }
-    ]
+	extend : 'Ext.panel.Panel',
+	frame : true,
+	layout : 'border',
+
+	items : [ {
+		xtype : 'toolbar',
+		region : 'north',
+		height : 30,
+		items : [ {
+			text : 'System',
+			menu : {
+				xtype : 'menu',
+				plain : true,
+				items : [ {
+					text : 'Ganti Password'
+				}, {
+					text : 'Logout'
+				} ]
+			}
+		}, {
+			text : 'Master Data',
+			menu : {
+				xtype : 'menu',
+				plain : true,
+				items : [ {
+					text : 'User'
+				}, {
+					text : 'Role'
+				}, {
+					text : 'Permission'
+				} ]
+			}
+		}, {
+			text : 'Transaksi'
+		}, {
+			text : 'Report'
+		} ]
+	}, {
+		xtype : 'panel',
+		title : 'Information',
+		collapsible : true,
+		region : 'east',
+		width : 300
+	}, {
+		xtype : 'toolbar',
+		region : 'south',
+		height : 20,
+		items : [ {
+			text : 'current user : endy',
+			xtype : 'label'
+		} ]
+	}, {
+		id : 'centerpanel',
+		xtype : 'tabpanel',
+		region : 'center',
+		items : [ 
+	         {
+				title : 'Dashboard',
+				bodyPadding : 10,
+				html : 'Selamat Datang di Aplikasi Belajar'
+			} 
+		]
+	} ]
 });
